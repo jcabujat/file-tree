60edb4f1976a95161d9462cb9aba50ce5d5489ab
jonathancabujat <jonathancabujat@gmail.com>
2020-10-15 11:36:39 +0800

    03: Code for iterating contents of a directory using a customized filter.

--- src/com/jcabujat/Main.java
+++ src/com/jcabujat/Main.java
@@ -7,9 +7,17 @@ public class Main {
 
     public static void main(String[] args) {
         Path directory = FileSystems.getDefault().getPath("FileTree\\Dir2");
-        try (DirectoryStream<Path> contents = Files.newDirectoryStream(directory,"*.dat")) { // filtering the content to get files with .dat extension
+        DirectoryStream.Filter<Path> filter = Files::isRegularFile; // method reference
+//        DirectoryStream.Filter<Path> filter = entry -> (Files.isRegularFile(entry)); // lambda equivalent of the below code
+//        DirectoryStream.Filter<Path> filter = new DirectoryStream.Filter<Path>() {
+//            @Override
+//            public boolean accept(Path entry) throws IOException {
+//                return (Files.isRegularFile(entry));
+//            }
+//        };
+        try (DirectoryStream<Path> contents = Files.newDirectoryStream(directory, filter)) { // filtering the content using the customized filter object
 
-            for (Path file: contents) {
+            for (Path file : contents) {
                 System.out.println(file.getFileName());
             }
 
