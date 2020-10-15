6adc70b3cca0d99c1d17f259e67aa2c6ad19cd7a
jonathancabujat <jonathancabujat@gmail.com>
2020-10-15 11:27:31 +0800

    01: Code for iterating contents of a directory.

---
+++ src/com/jcabujat/Main.java
@@ -0,0 +1,20 @@
+package com.jcabujat;
+
+import java.io.IOException;
+import java.nio.file.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Path directory = FileSystems.getDefault().getPath("FileTree\\Dir2");
+        try (DirectoryStream<Path> contents = Files.newDirectoryStream(directory)) {
+
+            for (Path file: contents) {
+                System.out.println(file.getFileName());
+            }
+
+        } catch (IOException | DirectoryIteratorException e) {
+            e.printStackTrace();
+        }
+    }
+}
