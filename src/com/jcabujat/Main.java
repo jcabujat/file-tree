bc5d9d965de027915e3846cfe1c3ae3ed1e9d413
jonathancabujat <jonathancabujat@gmail.com>
2020-10-15 11:28:56 +0800

    02: Code for iterating contents of a directory with filter.

--- src/com/jcabujat/Main.java
+++ src/com/jcabujat/Main.java
@@ -7,7 +7,7 @@ public class Main {
 
     public static void main(String[] args) {
         Path directory = FileSystems.getDefault().getPath("FileTree\\Dir2");
-        try (DirectoryStream<Path> contents = Files.newDirectoryStream(directory)) {
+        try (DirectoryStream<Path> contents = Files.newDirectoryStream(directory,"*.dat")) { // filtering the content to get files with .dat extension
 
             for (Path file: contents) {
                 System.out.println(file.getFileName());
