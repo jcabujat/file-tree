c4f9601c18f27384d7c61210e65344e2c4eb9a2c
jonathancabujat <jonathancabujat@gmail.com>
2020-10-15 11:45:54 +0800

    03: Using file separator for real-world application to avoid hard-coding of file separator which is different between Mac/Linux and Windows.

--- src/com/jcabujat/Main.java
+++ src/com/jcabujat/Main.java
@@ -1,12 +1,13 @@
 package com.jcabujat;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.*;
 
 public class Main {
 
     public static void main(String[] args) {
-        Path directory = FileSystems.getDefault().getPath("FileTree\\Dir2");
+        Path directory = FileSystems.getDefault().getPath("FileTree" + File.separator + "Dir2");
         DirectoryStream.Filter<Path> filter = Files::isRegularFile; // method reference
 //        DirectoryStream.Filter<Path> filter = entry -> (Files.isRegularFile(entry)); // lambda equivalent of the below code
 //        DirectoryStream.Filter<Path> filter = new DirectoryStream.Filter<Path>() {
@@ -24,5 +25,10 @@ public class Main {
         } catch (IOException | DirectoryIteratorException e) {
             e.printStackTrace();
         }
+
+        String separator = File.separator;
+        System.out.println(separator);
+        separator = FileSystems.getDefault().getSeparator();
+        System.out.println(separator);
     }
 }
